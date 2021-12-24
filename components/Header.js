d87bdf27b6475e8af43aaacfb455aa1fb16644f8
Joseph Hunter <jrhunt1706@gmail.com>
2021-12-23 21:32:38 -0600

    only displays footer

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,6 @@ import html from "html-literal";
 
 export default () => html`
   <header>
-    <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
