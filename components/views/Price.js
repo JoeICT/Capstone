7237281b418637f454b39c8029a721540e9eecee
Joseph Hunter <jrhunt1706@gmail.com>
2021-12-29 21:19:08 -0600

    sqft calculator update

--- components/views/Price.js
+++ components/views/Price.js
@@ -1,24 +1,27 @@
 import html from "html-literal";
 
 export default () => html`
+  <div class="flexbox-item flexbox-item-3">
+    <h1>Calculate your Square Footage</h1>
+    <div class="length_wrapper">
+      <h3>Length</h3>
 
-<div class="width_wrapper">
-  <h3>Width</h3>
+      <input type="text" id="length_feet" name="length_feet" />
+      <label for="length_feet">feet</label>
+    </div>
 
-  <input type="text" id="width_feet" name="width_feet" >
-  <label for="width_feet">feet</label>
+    <div class="width_wrapper">
+      <h3>Width</h3>
 
+      <input type="text" id="width_feet" name="width_feet" />
+      <label for="width_feet">feet</label>
+    </div>
+    <div class="total_wrapper">
+      <h3>Total</h3>
+      <input type="text" id="total" name="total" />
+      <label for="total">sq ft</label>
+    </div>
 
-</div>
-<div class="total_wrapper">
-  <h3>Total</h3>
-  <input type="text" id="total" name="total" >
-  <label for="total">sq ft</label>
-</div>
-
-<button type="button" id="calculate" name="button">Calculate</button>
-
-
-</div>
-
+    <button type="button" id="calculate" name="button">Calculate</button>
+  </div>
 `;
