fa92aa79947b96a77443df511e7ac2663ac8d7d2
Joseph Hunter <jrhunt1706@gmail.com>
2021-12-30 19:07:37 -0600

    change home.js to state, add js weather

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,6 +1,14 @@
 import html from "html-literal";
 
-export default () => html`
+const kelvinToFahrenheit = kelvinTemp =>
+  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+export default st => html`
+  <h3>
+    Temperature in ${st.weather.city} is
+    ${kelvinToFahrenheit(st.weather.temp)}F. It feels like
+    ${kelvinToFahrenheit(st.weather.feelsLike)}F.
+  </h3>
   <div class="flexbox-item flexbox-item-3">
     <h3>Some paragraph about anything that will be in the body of my page.</h3>
   </div>
@@ -9,12 +17,3 @@ export default () => html`
   </div>
 `;
 // // Add the Kelvin converter const kelvinToFahrenheit = kelvinTemp => const
-//   kelvinToFahrenheit = kelvinTemp => Math.round((kelvinTemp - 273.15) * (9 / 5)
-//   + 32);
-//   export default st => html`
-//   <h3>
-//     Temperature in ${st.weather.city} is
-//     ${kelvinToFahrenheit(st.weather.temp)}F. It feels like
-//     ${kelvinToFahrenheit(st.weather.feelsLike)}F.
-//   </h3>
-// `;
