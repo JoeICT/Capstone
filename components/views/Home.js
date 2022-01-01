d226e307fe3bc0792dde34d65e405d68a66333ea
Joseph Hunter <jrhunt1706@gmail.com>
2022-01-01 08:08:54 -0600

    add lorem ipsum to home

--- components/views/Home.js
+++ components/views/Home.js
@@ -2,10 +2,42 @@ import html from "html-literal";
 
 export default () => html`
   <div class="flexbox-item flexbox-item-3">
-    <h3>Some paragraph about anything that will be in the body of my page.</h3>
+    <h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien
+      est, dictum et risus et, fringilla euismod dolor. Nulla auctor a risus ut
+      dapibus. In eu efficitur risus, ac lobortis lorem. Quisque porta dictum
+      enim, eu imperdiet leo scelerisque vitae. Morbi placerat ante eget augue
+      varius, ac placerat ante sagittis. Donec in sem vel nibh sodales auctor.
+      Sed id ultricies arcu. Integer sollicitudin convallis ante, nec volutpat
+      lorem mattis sed. Donec iaculis felis a ante maximus pellentesque. Lorem
+      ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea
+      dictumst. Suspendisse rhoncus imperdiet enim, vitae tincidunt mauris
+      faucibus ut. Integer ultrices tempus mi, at fermentum arcu lacinia eget.
+      Donec vitae tristique eros. Integer lacinia molestie eleifend. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Morbi tempus tempor cursus. Quisque rutrum tristique imperdiet. Duis
+      vulputate orci orci, id pretium mauris tincidunt et. Quisque vitae orci
+      accumsan lectus gravida tincidunt a eu purus. Suspendisse suscipit
+      ultrices sapien, nec pellentesque risus ornare sit amet. Morbi a hendrerit
+      nibh. Cras vitae orci pulvinar, tincidunt ipsum a, pellentesque mauris.
+      Vivamus aliquam ac nibh euismod imperdiet. Etiam fermentum malesuada elit
+      sollicitudin rhoncus.
+    </h3>
   </div>
   <div class="flexbox-item flexbox-item-4">
-    <h3>Some paragraph about anything that will be in the body of my page.</h3>
+    <h3>
+      Donec pellentesque sollicitudin vestibulum. Nullam id nisi arcu. Integer
+      dignissim massa sit amet felis faucibus, eu dictum orci congue. Fusce
+      auctor finibus urna, eu convallis nisl lacinia id. Quisque purus ligula,
+      ullamcorper id diam quis, aliquam pellentesque erat. Cras rhoncus lobortis
+      ex a pellentesque. Donec sit amet nunc id arcu rutrum tempus. Vestibulum
+      nec pretium mauris. Cras luctus ultrices turpis, in maximus quam posuere
+      eu. Sed sapien arcu, vehicula nec justo et, luctus accumsan odio. Duis
+      lobortis justo dui, sed varius tellus mollis et. Morbi tempor venenatis
+      ante, et lobortis quam efficitur sed. Mauris a lacus aliquam, sollicitudin
+      nibh a, ullamcorper nulla. Mauris vel tempor mauris. Suspendisse eu auctor
+      dui. Donec faucibus semper lorem non venenatis.
+    </h3>
   </div>
 `;
 // // Add the Kelvin converter const kelvinToFahrenheit = kelvinTemp => const
