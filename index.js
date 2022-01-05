2357a7efb39aef36e3c5c060557f23e4cce17030
Joseph Hunter <jrhunt1706@gmail.com>
2022-01-05 14:45:50 -0600

    add axios / center headline / add router hooks

--- index.js
+++ index.js
@@ -25,8 +25,11 @@
 
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
+import axios from "axios";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import dotenv from "dotenv";
+dotenv.config();
 //declare a function named render
 
 const router = new Navigo(window.location.origin);
@@ -57,9 +60,42 @@ function addEventListeners(st) {
   });
 }
 // router hooks
+
+// router
+//   .on({
+//     ":page": params => render(state[capitalize(params.page)]),
+//     "/": () => render(state.Home)
+//   })
+//   .resolve();
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+});
+
+//ADD ROUTER HERE ...
 router
   .on({
-    ":page": params => render(state[capitalize(params.page)]),
-    "/": () => render(state.Home)
+    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)])
   })
   .resolve();
