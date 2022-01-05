f662e62143d205cdf085349d4bc544e356a48e8d
Joseph Hunter <jrhunt1706@gmail.com>
2022-01-05 15:42:16 -0600

    comment out top nav / delete unnesecary comments

--- components/Header.js
+++ components/Header.js
@@ -4,36 +4,18 @@ import logo from "../assets/image/new1.jpg";
 
 export default () => html`
   <header>
-
     <!-- Top Navigation -->
-    <div class="topnav">
-      <a href="#home" class="active"></a>
-      <!-- Navigation links -->
+    <!-- <div class="topnav">
+      <a href="#home" class="active"></a> -->
+    <!-- Navigation links -->
 
-      <div id="myLinks">
-        <a href="#resources">Resources</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="#price.html">Price Your Job</a>
-        <a href="#find.html">Find A Contractor</a>
-
-        <!-- <div class="flexbox-item flexbox-item-3"></div> -->
-      </div>
-
-      <!-- "Hamburger menu"-->
-
-
-      <!-- Top Navigation Menu -->
-
-      <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
-      <!-- <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
-      </a>
+    <!-- <div id="myLinks">
+      <a href="#resources">Resources</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+      <a href="#price.html">Price Your Job</a>
+      <a href="#find.html">Find A Contractor</a>
     </div> -->
-    <!-- "hamburger menu"-->
-    <!-- <script language="javascript" type="text/javascript"></script> -->
-
-    <!-- hamburger menu-->
 
     <div class="headerBar">
       <div class="headerImage">
--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,3 @@
-/* eslint-disable prettier/prettier */
-// import * as views from "./views";
-
-// export default () => `
-// ${views["Home"]()}, ${views["Find"]()}, ${views["Price"]()}, ${views[
-//   "Resource"
-// ]()}
-// `;
 import html from "html-literal";
 import * as views from "./views";
 
@@ -13,12 +5,11 @@ export default st => html`
   ${views[st.view](st)}
 
   <nav>
-  <div class="navbar">
-  <a href="#Home." style="color:black">Home  /</a>
-  <a href="#Resources." style="color:black">Resources  /</a>
-  <a href="#Price." style="color:black">Price your job  /</a>
-  <a href="#Find." style="color:black">Find a Contractor</a>
-</div>
-
-</nav>
+    <div class="navbar">
+      <a href="#Home." style="color:black">Home /</a>
+      <a href="#Resources." style="color:black">Resources /</a>
+      <a href="#Price." style="color:black">Price your job /</a>
+      <a href="#Find." style="color:black">Find a Contractor</a>
+    </div>
+  </nav>
 `;
