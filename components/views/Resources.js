5f1a4e9653ceedae0e400e953b9e5d9a12eaec4b
Joseph Hunter <jrhunt1706@gmail.com>
2022-01-08 15:35:27 -0600

    fix padding login / css updates

--- components/views/Resources.js
+++ components/views/Resources.js
@@ -2,28 +2,31 @@ import html from "html-literal";
 
 export default () => html`
   <head>
-    <div class="reshead"><h1>Resources</h1></div>
+    <div class="reshead"><h1><strong>Resources</strong></h1></div>
     <div class="flex-container">
       <div id="res1">
         WatchDog Offender Search
+
         <p><h6>
+          <strong>
           This is where you can take a closer look into the person you are
-          allowing in your home and potentially around your children.
+          allowing in your home and potentially around your children.</strong>
           <br>
           <a href="https://www.familywatchdog.us/">Offender Search!</a>
           </h6>
         </p>
       </div>
       <div id="res2">Horror Stories
-        <p><h6>
-          Learn what to watch out for and hopefully avoid by reading some of these stories from customers who had bad experiences with the contractor that they hired.
+        <p><h6><strong>
+          Learn what to watch out for and hopefully avoid by reading some of these stories from customers who had bad experiences with the contractor that they hired.</strong>
         </h6>
         </p>
 
       </div>
       <div id="res3">Red Flags
-        <p><h6>
+        <p><h6><strong>
           Here you can read about "red flags", or things to watch out for when hiring a contractor.
+          </strong>
         </h6></p>
       </div>
     </div>
