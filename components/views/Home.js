f658869c53a1ef1b59bf8033134f30edc07a5699
Joseph Hunter <jrhunt1706@gmail.com>
2022-01-09 23:59:34 -0600

    css updates homepage / resources

--- components/views/Home.js
+++ components/views/Home.js
@@ -12,18 +12,18 @@ export default st => html`
     ${kelvinToFahrenheit(st.weather.feelsLike)}F.
   </h2>
   <div id="home-body">
-  <div class="welcomegreeting"><h1>About This Site</h1></div><br>
-  <div class="home-page-par1">
+  <div class="welcomegreeting"><h1><span>About This Site</span></h1></div><br>
+  <div class="home-page-par1"><br><br>
     <h2>
-      <strong>
+      <strong><span>
     Renovating your home is a big project, whether you're tackling just one room or embarking on a more extensive makeover. If you're only updating a few fixtures or surfaces, you might be able to do most of the work yourself or bring in specific pros for tasks like tiling or installing cabinetry. But if the project involves a complete overhaul of the room, hiring a general contractor might be worth the extra cost. As experts in building and remodeling projects, contractors can manage all the necessary permits, inspections, labor, equipment, and materials to ensure your remodel goes off without a hitch. The key is to find a reputable contractor that can correctly and efficiently meet the needs of your project. Make your remodel a success with these tips on how to find the right contractor.
-    </strong>
+    </span></strong>
   </div>
     </h2>
-
+<br><br>
   <div class="home-page-par2">
-    <h2><strong>
-    Plan to gather at least three estimates from various contractors before you make a decision. More quotes (say five or six) will give you a clearer picture of how these companies stack up against each other. Not only will you get more perspective on appropriate costs, but also you'll get an idea of the caliber of work that each contractor will provide. For example, one contractor might offer a bargain rate and marginal work; another's high bid could indicate quality.
+    <h2><strong><span>
+    Plan to gather at least three estimates from various contractors before you make a decision. More quotes (say five or six) will give you a clearer picture of how these companies stack up against each other. Not only will you get more perspective on appropriate costs, but also you'll get an idea of the caliber of work that each contractor will provide. For example, one contractor might offer a bargain rate and marginal work; another's high bid could indicate quality.</span>
     </strong>
     </h2>
   </div>
--- components/views/Resources.js
+++ components/views/Resources.js
@@ -5,29 +5,30 @@ export default () => html`
     <div class="reshead"><h1><strong>Resources</strong></h1></div>
     <div class="flex-container">
       <div id="res1">
-        <strong>WatchDog Offender Search</strong>
-
-        <p><h6>
-          <strong>
+      <br><br><br><br>
+        <strong><span>WatchDog Offender Search</span></strong>
+        <br><br><br>
+        <p><h3>
+          <strong><span>
           This is where you can take a closer look into the person you are
-          allowing in your home and potentially around your children.</strong>
-          <br>
+          allowing in your home and potentially around your children.</span></strong>
+          <br><br>
           <a href="https://www.familywatchdog.us/"><span>Offender Search!</span></a>
-          </h6>
+          </h3>
         </p>
         <div class="resimg2"></div>
       </div>
-      <div class="res2"><strong>Horror Stories</strong><p>
-          Learn what to watch out for and hopefully avoid by reading some of these stories from customers who had bad experiences with the contractor that they hired.</strong>
+      <div class="res2"><h4><strong>Horror Stories</strong></h4><p>
+          Learn what to watch out for and hopefully avoid by reading some of these stories from customers who had bad experiences with the contractor that they hired.
         </p></div>
         <div class="resimg"></div>
 
       </div>
-      <div id="res3"><strong>Red Flags</strong>
-        <p><h6><strong>
+      <div id="res3"><h4><strong>Red Flags</strong></h4>
+        <p>
           Here you can read about "red flags", or things to watch out for when hiring a contractor.
-          </strong>
-        </h6></p>
+
+        </p>
       </div>
     </div>
   </head>
